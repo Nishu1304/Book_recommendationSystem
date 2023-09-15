423af95c004d194240c2a102f48104cce9b4adfe
Nishu13 <126399307+Nishu1304@users.noreply.github.com>
2023-09-15 13:04:48 +0530

    Add files via upload

---
+++ new_file2.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+
+books = pd.read_csv('Books.csv')
+users = pd.read_csv('Users.csv')
+ratings = pd.read_csv('Ratings.csv')
+ratings_with_name = ratings.merge(books, on='ISBN')
+
+x = ratings_with_name.groupby('User-ID').count()['Book-Rating'] > 200
+padhe_likhe_users = x[x].index
+filtered_rating = ratings_with_name[ratings_with_name['User-ID'].isin(padhe_likhe_users)]
+y = filtered_rating.groupby('Book-Title').count()['Book-Rating'] >= 50
+famous_books = y[y].index
+final_ratings = filtered_rating[filtered_rating['Book-Title'].isin(famous_books)]
+pt = final_ratings.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating')
+pt.fillna(0, inplace=True)
+
+similarity_scores = cosine_similarity(pt)
+
+
+def recommend(book_name):
+    # index fetch
+    index = np.where(pt.index == book_name)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+
+    return data
+
+
+books.drop_duplicates('Book-Title')
+pickle.dump(pt, open('pt.pkl', 'wb'))
+pickle.dump(books, open('books.pkl', 'wb'))
+pickle.dump(similarity_scores, open('similarity_scores.pkl', 'wb'))
